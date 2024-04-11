2e9f1cf42349b28b1b6212959bc956b2569641f4
Shivam kumar <49615953+shivamjamshedpur@users.noreply.github.com>
2024-04-11 21:37:36 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
 function makeid(l) {
   // write your code here
+	const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  let result = '';
+  for (let i = 0; i < l; i++) {
+    result += characters.charAt(Math.floor(Math.random() * characters.length));
+  }
+  return result;
 }
 
 // Do not change the code below.
